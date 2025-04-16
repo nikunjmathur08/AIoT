2ad692af4d23d00214f14e28b94733f1fa505ba2
Nikunj Mathur <nikunjmathur0810@gmail.com>
2025-04-16 10:41:21 +0530

    fix: remove unnecessary imports

--- app/(tabs)/camera.tsx
+++ app/(tabs)/camera.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { CameraView, useCameraPermissions, CameraType } from "expo-camera";
